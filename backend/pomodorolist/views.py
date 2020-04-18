71e5ec858b9c099deb79b44fa3f8444b71b14288
ayciceksamet <ayciceksamet@gmail.com>
2020-04-19 00:10:15 +0300

    serializers added

--- backend/pomodorolist/views.py
+++ backend/pomodorolist/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PomodoroListSerializer
+from .models import PomodoroList
+
+class PomodoroListView(viewsets.ModelViewSet):
+    serializer_class = PomodoroListSerializer
+    queryset = PomodoroList.objects.all()
 
 # Create your views here.
